be26b7804e1b0a01a7c61a7b42ab0d02380aa7b8
fantacola0x0 <96630960+fantacola0x0@users.noreply.github.com>
2023-10-11 00:42:16 +0700

    appjs fix commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Films from './Films';
 import Contact from './Contact';
